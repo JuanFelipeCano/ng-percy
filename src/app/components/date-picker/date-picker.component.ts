e6183e733f7caa5477ee00acb1a7f70e4613648e
Juan Betancur <juanfelipebetancurcano@gmail.com>
2024-10-05 09:57:59 -0500

    created models

--- src/app/components/date-picker/date-picker.component.ts
+++ src/app/components/date-picker/date-picker.component.ts
@@ -1,44 +1,13 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, model, OnInit, output, signal, WritableSignal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, model, OnInit, output } from '@angular/core';
 import { DateTime as Luxon, Info as LuxonInfo } from 'luxon';
-
-interface CalendarDay {
-  date: Date;
-  isFromAnotherMonth: boolean;
-}
-
-/**
- * DatePickerConfig
- * @see https://moment.github.io/luxon/#/formatting?id=table-of-tokens for date format
- * @param format - Date format
- * @param date - Initial date
- * @param locale - Locale
- * @param minDate - Minimum date
- * @param maxDate - Maximum date
- * @param disabledDates - Disabled dates
- */
-interface DatePickerConfig {
-  format: string;
-  date?: Date;
-  locale?: string;
-  disabled?: boolean;
-  // out of scope
-  minDate?: Date;
-  maxDate?: Date;
-  disabledDates?: Date[];
-}
-
-interface DatePicker {
-  visualDate: string;
-  date: Date;
-}
+import { CalendarDay, DatePicker, DatePickerConfig } from './models';
 
 @Component({
   selector: 'app-date-picker',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [ CommonModule ],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss',
   host: { 'class': 'date-picker' },
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,1 @@
-export * from './calendar-picker/calendar-picker.component';
 export * from './date-picker/date-picker.component';
