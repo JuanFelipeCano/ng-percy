356909cba0af7c009216b423d77d6d3a8bfdc2cd
Juan Betancur <juanfelipebetancurcano@gmail.com>
2024-10-22 21:56:23 -0500

    theme automathic change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatePickerComponent } from './components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatePickerComponent],
+  imports: [ RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-components';
+export class AppComponent implements OnInit {
+
+  public ngOnInit(): void {
+    this.setTheme();
+    this.subscribeToThemeChange();
+  }
+
+  private setTheme(): void {
+    const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark' : 'light';
+
+    document.querySelector('body')!.className = colorScheme;
+  }
+
+  private subscribeToThemeChange(): void {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      this.setTheme();
+    });
+  }
 }
