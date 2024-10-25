7497a27f2a35ff8d18986c9c55419076b69d2b04
Juan Betancur <juanfelipebetancurcano@gmail.com>
2024-10-25 12:43:19 -0500

    initial app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,20 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatePickerComponent } from './components';
-import { DatePickerConfig } from './components/date-picker/models';
 import { DialogScreenController } from './components/dialog-screen/dialog-screen.controller';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ RouterOutlet ],
+  providers: [ DialogScreenController ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
 
   constructor(
-    // public modalService: ModalService
-    public dialogScreenController: DialogScreenController
+    public readonly _dialogScreenController: DialogScreenController
   ) {
-    this.openModal()
-  }
-
-  openModal() {
-    const ref =this.dialogScreenController.create({
-      component: DatePickerComponent,
-      componentProps: {
-        config: signal<DatePickerConfig>({ format: 'DDD', date: new Date(2024, 0, 1) }),
-      },
-    }).subscribe((result) => {
-      console.log('Resultado del modal:', result);
-      // this.dialogScreenController.close();
-    });
   }
 
   public ngOnInit(): void {
