ae42921ad1764cee8c876969ff17b39c1d789829
Juan Betancur <juanfelipebetancurcano@gmail.com>
2024-12-06 19:21:39 -0500

    refactor option item component

--- projects/percy/src/lib/components/option-item/option-item.component.ts
+++ projects/percy/src/lib/components/option-item/option-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { booleanAttribute, Component, input, output } from '@angular/core';
 import { DropdownListOption } from '../../../public-api';
 import { ShapeBase } from '../../types';
 
@@ -16,18 +16,25 @@ type OptionItemShape = ShapeBase;
     '[class.percy-option-item_square]': 'shape() === "square"',
     '[class.percy-option-item_circle]': 'shape() === "circle"',
     '[class.percy-option-item_selected]': 'selected()',
+    '[class.percy-option-item_disabled]': 'disabled()',
+    '[class.percy-option-item_focused]': 'focused()',
+    '[attr.data-value]': 'option().value',
+    '[attr.data-text]': 'option().text',
   },
 })
 export class PercyOptionItemComponent {
 
   public readonly option = input.required<DropdownListOption>();
-  public readonly selected = input<boolean>(false);
-  public readonly disabled = input<boolean>(false);
+  public readonly selected = input(false, { transform: booleanAttribute });
+  public readonly disabled = input(false, { transform: booleanAttribute });
+  public readonly focused = input(false, { transform: booleanAttribute });
   public readonly shape = input<OptionItemShape>('round');
 
   public readonly onSelected = output<DropdownListOption>();
 
   public selectOption(): void {
+    if (this.disabled()) return;
+
     const option = {
       ...this.option(),
       selected: !this.option().selected,
