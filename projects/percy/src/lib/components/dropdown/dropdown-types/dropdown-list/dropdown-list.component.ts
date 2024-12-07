c1f44994738d3181bd9a55fb34d68f5800ef78d1
Juan Betancur <juanfelipebetancurcano@gmail.com>
2024-12-06 19:24:21 -0500

    a11y features for dropdown list component

--- projects/percy/src/lib/components/dropdown/dropdown-types/dropdown-list/dropdown-list.component.ts
+++ projects/percy/src/lib/components/dropdown/dropdown-types/dropdown-list/dropdown-list.component.ts
@@ -3,15 +3,20 @@ import {
   Component,
   computed,
   CUSTOM_ELEMENTS_SCHEMA,
+  HostListener,
+  inject,
   input,
   OnInit,
   output,
   Signal,
 } from '@angular/core';
 import { ShapeBase } from '../../../../types';
-import { randomId } from '../../../../utils';
+import { randomId, sleep } from '../../../../utils';
 import { PercyOptionItemComponent } from '../../../option-item/option-item.component';
 import { PercyDropdownComponent } from '../../dropdown/dropdown.component';
+import { FocusService, KeyboardExecutorService } from '../../../../services';
+import { KeyboardKeys, ONE, TEN, ZERO } from '../../../../constants';
+import { CommonModule } from '@angular/common';
 
 export interface DropdownListOption {
   value: string;
@@ -25,7 +30,8 @@ type DropdownListShape = ShapeBase;
 @Component({
   selector: 'percy-dropdown-list',
   standalone: true,
-  imports: [ PercyDropdownComponent, PercyOptionItemComponent ],
+  imports: [ CommonModule, PercyDropdownComponent, PercyOptionItemComponent ],
+  providers: [ FocusService, KeyboardExecutorService ],
   templateUrl: './dropdown-list.component.html',
   styleUrl: './dropdown-list.component.scss',
   host: {
@@ -49,15 +55,23 @@ export class PercyDropdownListComponent implements OnInit {
   /**
    * A11y properties
    */
-  public readonly a11yBadgeAriaLabel = input<string | null>(null, { alias: 'badge-aria-label' });
-  public readonly a11yClearButtonAriaLabel = input<string | null>(null, { alias: 'clear-button-aria-label' });
+  public readonly a11yBadgeAriaLabel = input<string | null>(null, { alias: 'a11y-badge-aria-label' });
+  public readonly a11yClearButtonAriaLabel = input<string | null>(null, { alias: 'a11y-clear-button-aria-label' });
+  public readonly a11ySelectedText = input<string | null>(null, { alias: 'a11y-selected-text' });
+  public readonly a11yNotSelectedText = input<string | null>(null, { alias: 'a11y-not-selected-text' });
+  public readonly a11yOfText = input<string | null>(null, { alias: 'a11y-of-text' });
 
   public readonly percyChange = output<DropdownListOption | DropdownListOption[]>();
 
-  public isOpen!: boolean;
+  protected isOpen!: boolean;
   protected _options!: Signal<DropdownListOption[]>;
   protected counter!: number;
   protected selectedOptions!: DropdownListOption | DropdownListOption[] | undefined;
+  protected focusedOption!: DropdownListOption | undefined;
+  protected lastSelectedOption!: DropdownListOption | undefined;
+
+  private readonly _focusService = inject(FocusService);
+  private readonly _keyboardExecutor = inject(KeyboardExecutorService);
 
   constructor() {
     this.isOpen = false;
@@ -69,6 +83,36 @@ export class PercyDropdownListComponent implements OnInit {
     return (this.selectedOptions as DropdownListOption)?.text;
   }
 
+  public get labelId(): string {
+    return this.id() + '-label';
+  }
+
+  public get a11yFocusedOptionText(): string {
+    if (!this.focusedOption) return '';
+
+    const currentIndex = this._options().findIndex(option => option.value === this.focusedOption?.value);
+    const counter = `${ currentIndex + ONE } ${ this.a11yOfText() ?? '/' } ${ this._options().length }`;
+    const middleText = !this.focusedOption.selected
+      ? this.a11yNotSelectedText() ?? ' '
+      : this.a11ySelectedText() ?? ' ';
+
+    return `${ this.focusedOption.text } ${ middleText } ${ counter }`;
+  }
+
+  public get a11ySelectedOptionText(): string {
+    if (!this.lastSelectedOption || this.isOpen) return '';
+
+    if (this.multiple()) {
+      return `
+        ${ this.a11ySelectedText() ?? '' }
+        ${ this.counter } ${ this.a11yOfText() ?? '/' }
+        ${ this._options().length }
+      `;
+    }
+
+    return `${ this.lastSelectedOption.text } ${ this.a11ySelectedText() ?? '' }`;
+  }
+
   public ngOnInit(): void {
     this.setSelectedOptions();
     this.upateCounter();
@@ -76,10 +120,21 @@ export class PercyDropdownListComponent implements OnInit {
 
   public toggleDropdown(): void {
     this.isOpen = !this.isOpen;
+    this.focusedOption = undefined;
+
+    if (this.isOpen) {
+      this.lastSelectedOption = undefined;
+      this._focusService.setLastFocusedElement();
+      this.setFocusedOption();
+    } else {
+      this._focusService.setFocusToLastFocusedElement();
+    }
   }
 
-  public selectOption(option: DropdownListOption): void {
-    if (this.disabled()) return;
+  public selectOption(option: DropdownListOption | undefined): void {
+    if (this.disabled() || !option || option.disabled) return;
+
+    this.lastSelectedOption = option;
 
     this.multiple() ? this.setSelectedMultipleOptions(option) : this.setSelectedOption(option);
 
@@ -94,7 +149,6 @@ export class PercyDropdownListComponent implements OnInit {
     this._options = computed(() => newOptions);
     this.percyChange.emit([]);
     this.upateCounter();
-    this.toggleDropdown();
   }
 
   private setSelectedOption(option: DropdownListOption): void {
@@ -131,6 +185,98 @@ export class PercyDropdownListComponent implements OnInit {
       this.selectedOptions = this._options().filter(item => item.selected);
     } else {
       this.selectedOptions = this._options().find(item => item.selected);
+    }
+  }
+
+  /**
+   * A11y configuration
+   */
+
+  @HostListener('keydown', ['$event'])
+  public onKeyDown(event: KeyboardEvent): void {
+    const KeysMapper = {
+      [KeyboardKeys.ESCAPE]: () => this.toggleDropdown(),
+      [KeyboardKeys.ESC]: () => this.toggleDropdown(),
+      [KeyboardKeys.DOWN]: () => this.setNextFocusedOption(),
+      [KeyboardKeys.ARROW_DOWN]: () => this.setNextFocusedOption(),
+      [KeyboardKeys.UP]: () => this.setPreviousFocusedOption(),
+      [KeyboardKeys.ARROW_UP]: () => this.setPreviousFocusedOption(),
+      [KeyboardKeys.ENTER]: () => this.toggleDropdownOrSelectOption(this.focusedOption),
+      [KeyboardKeys.SPACE]: () => this.toggleDropdownOrSelectOption(this.focusedOption),
+      [KeyboardKeys.HOME]: () => this.setFirstFocusedOption(),
+      [KeyboardKeys.END]: () => this.setLastFocusedOption(),
+    };
+
+    this._keyboardExecutor.execute(KeysMapper, event);
+  }
+
+  private toggleDropdownOrSelectOption(option: DropdownListOption | undefined): void {
+    this.isOpen ? this.selectOption(option) : this.toggleDropdown();
+  }
+
+  private async setFocusedOption(): Promise<void> {
+    this.focusedOption = this._options().find(option => option.selected);
+    await sleep(TEN);
+    this.scrollToFocusedOption();
+  }
+
+  private setNextFocusedOption(): void {
+    this.toggleDropdownOrSelectOption(undefined);
+
+    if (!this.focusedOption) {
+      this.focusedOption = this._options().find(option => !option.disabled);
+      return;
+    }
+
+    const currentIndex = this._options().findIndex(option => option.value === this.focusedOption?.value);
+
+    if (currentIndex < this._options().length - ONE) {
+      this.focusedOption = this._options().find((option, index) => !option.disabled && index > currentIndex);
+    }
+
+    this.scrollToFocusedOption();
+  }
+
+  private setPreviousFocusedOption(): void {
+    this.toggleDropdownOrSelectOption(undefined);
+
+    if (!this.focusedOption) return;
+
+    const currentIndex = this._options().findIndex(option => option.value === this.focusedOption?.value);
+
+    if (currentIndex > ZERO) {
+      const option = this._options()
+        .slice(0, currentIndex)
+        .reverse()
+        .find(option => !option.disabled);
+
+      this.focusedOption = option || this.focusedOption;
+    }
+
+    this.scrollToFocusedOption();
+  }
+
+  private setFirstFocusedOption(): void {
+    this.focusedOption = this._options().find(option => !option.disabled);
+
+    this.scrollToFocusedOption();
+  }
+
+  private setLastFocusedOption(): void {
+    this.focusedOption = [ ...this._options() ].reverse().find(option => !option.disabled);
+
+    this.scrollToFocusedOption();
+  }
+
+  private scrollToFocusedOption(): void {
+    const focusedElement = document.querySelector('.percy-dropdown-list_list')
+      ?.querySelector(`[data-value="${this.focusedOption?.value}"]`);
+
+    if (focusedElement) {
+      focusedElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
     }
   }
 }
