1f58a2bc64e0119795ddf22eafa84e904e09f0f1
Juan Betancur <juanfelipebetancurcano@gmail.com>
2024-12-20 12:23:24 -0500

    button component tests

--- projects/percy/src/components/button/button.component.spec.ts
+++ projects/percy/src/components/button/button.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PercyButtonComponent } from './button.component';
 
-import { ButtonComponent } from './button.component';
-
-describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+describe('PercyButtonComponent', () => {
+  let component: PercyButtonComponent;
+  let fixture: ComponentFixture<PercyButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent]
+      imports: [ PercyButtonComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(PercyButtonComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('label', 'Click me');
     fixture.detectChanges();
   });
 
